224d0bc3171415cd1e0e8b127a129281d1878af0
TheRealDamiix <sionskies@gmail.com>
2025-06-18 01:11:21 -0400

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wocvglxoqxmbvdnydjym.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvY3ZnbHhvcXhtYnZkbnlkanltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyMTUwMzIsImV4cCI6MjA2NTc5MTAzMn0.YrCpoMpzI8SHN8kw_l2O8jb04KKIbYIXJTfaBRuTRgo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
